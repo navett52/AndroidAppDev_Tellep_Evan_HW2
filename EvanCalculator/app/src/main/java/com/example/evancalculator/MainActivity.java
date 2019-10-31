2d9cd322d508f768aeaae9f248ee4664dff0c654
Evan Tellep <evantellep@gmail.com>
2019-10-30 20:10:18 -0400

    Changed font size, gave padding, can extract opperator from string.

--- EvanCalculator/app/src/main/java/com/example/evancalculator/MainActivity.java
+++ EvanCalculator/app/src/main/java/com/example/evancalculator/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.evancalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,22 +37,56 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             Button button = (Button) v;
 
-            String action = button.getText().toString();
+            String action = null;
+            String operator = null;
 
-            if (button.getText().toString().matches("[0-9]")) {
+            if (button.getText().toString().matches("([0-9]+)")) {
                 action = "num";
+            }
+
+            if (button.getText().toString().matches("([\\+\\-\\*\\/])")) {
+                action = "operator";
+            }
+
+            if (button.getText().toString().equals("=")) {
+                action = "=";
             }
 
             switch(action) {
                 case "num":
                     display.append(button.getText());
                     break;
+                case "operator":
+                    if (operator == null) {
+                        display.append(button.getText());
+                        operator = button.getText().toString();
+                    }
+                    break;
                 case "=":
-
+                    getOpperator(display.getText().toString());
             }
         }
     };
 
-    //private float
+    private float solve(String operation) {
+        float result = 0;
+
+        // float num[] = operation.split('')
+
+        return result;
+    }
+
+    private char getOpperator(String operation) {
+        char operator;
+
+        if (operation.contains("+")) {
+            String components[] = operation.split("((?<=\\+)|(?=\\+))");
+
+            for (String piece : components) {
+                Log.d(this.getClass().getSimpleName(), piece);
+            }
+        }
+        return '~';
+    }
 
 }
